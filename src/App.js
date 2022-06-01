04971ad50c0f10b9523132368f88508a14c8ddbb
Victoria Khatian <khatian.v@gmail.com>
2022-06-01 11:41:03 +0100

    hero section styled

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
       <div className="hero">
         <div className="hero-info">
           <div className="hero-name">Victoria Khatian</div>
-          <div className="hero-bio">A short bio of the hero</div>
+          <div className="hero-bio">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
           <div className="button"></div>
         </div>
       </div>
