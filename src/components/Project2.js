5172ab315baf2f3e118ec46025fec7e19e6a602b
Victoria Khatian <khatian.v@gmail.com>
2022-06-05 11:41:45 +0100

    added section for proejct2

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -1,19 +1,25 @@
 import "./Project2.css";
+import imgFront from "../images/p2-img-front.png";
+import imgBack from "../images/p2-img-back.png";
 
 const Project2 = () => {
   return (
     <div className="project2">
       <div className="project2-images">
-        <div className="p2-image-back">image-back</div>
-        <div className="p2-image-front">image-front</div>
+        <div className="p2-image-back">
+          <img src={imgBack} alt="" />
+        </div>
+        <div className="p2-image-front">
+          <img src={imgFront} alt="" />
+        </div>
       </div>
       <div className="project2-info">
-        <div className="project2-notes">tech stack</div>
-        <div className="project2-title">Help Desk App</div>
+        <div className="project2-notes">React, Node, REST APIs</div>
+        <div className="project2-title">Help Desk</div>
         <div className="project2-paragraph">
-          Description of the app, how marketing and innovation teams leverage
-          design to turn disruptive technologies into real products that
-          generate revenue and earn consumer interest.
+          Website for the students to ask for help during workshops, allowing to
+          receive support from coaches or other students, track and analise
+          historical data for both those who asked and offered help.
         </div>
       </div>
     </div>
