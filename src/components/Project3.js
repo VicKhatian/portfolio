fd91dfbe5fe8e32ef861397aa961a38afd5bc0ae
Victoria Khatian <khatian.v@gmail.com>
2022-06-05 11:48:58 +0100

    added section project3

--- src/components/Project3.js
+++ src/components/Project3.js
@@ -1,17 +1,19 @@
 import "./Project3.css";
+import img from "../images/p3-image.png";
 
 const Project3 = () => {
   return (
     <div className="project3">
       <div className="project3-images">
-        <div className="p3-image">image</div>
+        <div className="p3-image">
+          <img src={img} alt="" />
+        </div>
       </div>
       <div className="project3-info">
-        <div className="project3-notes">tech stack</div>
-        <div className="project3-title">Help Desk App</div>
+        <div className="project3-notes">Vanilla Javascript, DOM</div>
+        <div className="project3-title">English dictionary</div>
         <div className="project3-paragraph">
-          short description of the project 3, description of the,description of
-          the,description of the
+          Simple yet elegant dictionary, with clean design and easy navigation.
         </div>
       </div>
     </div>
