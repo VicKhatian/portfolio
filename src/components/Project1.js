f8a0c7aaf04125e82720044033e3492528d2a4ca
Victoria Khatian <khatian.v@gmail.com>
2022-06-05 11:29:31 +0100

    added section fro project1

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,19 +1,24 @@
 import "./Project1.css";
+import imgFront from "../images/p1-img-front.png";
+import imgBack from "../images/p1-img-back.png";
 
 const Project1 = () => {
   return (
     <div className="project1">
       <div className="project1-images">
-        <div className="p1-image-back">image-back</div>
-        <div className="p1-image-front">image-front</div>
+        <div className="p1-image-back">
+          <img src={imgBack} alt="" />
+        </div>
+        <div className="p1-image-front">
+          <img src={imgFront} alt="" />
+        </div>
       </div>
       <div className="project1-info">
-        <div className="project1-notes">NODE TYPESCRIPT</div>
+        <div className="project1-notes">Javascript React JS</div>
         <div className="project1-title">Weather App</div>
         <div className="project1-paragraph">
-          Description of the app, how marketing and innovation teams leverage
-          design to turn disruptive technologies into real products that
-          generate revenue and earn consumer interest.
+          The app let users to check the weather on the chosen location, for 8
+          days ahead with ability to expand a selected date for the details.
         </div>
       </div>
     </div>
