84e00f38c766b6504f912613562a467d6c54974f
Victoria Khatian <khatian.v@gmail.com>
2022-06-21 13:51:13 +0100

    projectRight is fully responsive

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import "./Home.css";
 import Footer from "../../components/Footer/Footer";
 import Contact from "../../components/Contact/Contact";
 import ProjectCenter from "../../components/ProjectCenter/Project3";
-import ProjectRight from "../../components/ProjectRight/Project2";
+import ProjectRight from "../../components/ProjectRight/ProjectRight";
 import ProjectLeft from "../../components/ProjectLeft/ProjectLeft";
 import Hero from "../../components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
